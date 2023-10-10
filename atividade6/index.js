7bbbb0f251d68a7e2aa3ff6e3b096e23dbc6098d
kassya-rosa <81445258+kassya-rosa@users.noreply.github.com>
2023-10-10 20:18:49 -0300

    Create index.js

---
+++ atividade6/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+app.get('/', function(req, res){
+
+    let usuario = {
+        nome: 'Mario',
+        telefone: 2222222
+        endereco: 'Rua 25 norte, lt 20'
+        data: 
+    };
+    res.render('index.html', {usuario});
+});
+
+app.use(express.urlencoded({extended: true}));
+
+app.post('/dados', function(req,res){
+    let usuario = {
+        nome: req.body.nome,
+        telefone: req.body.telefone
+        endereco: req.body.endereco,
+        data: req.body.dta
+    };
+    res.render('dados.html', {usuario});
+});
+
+const PORT = 8080;
+
+app.listen(PORT, function(){
+    console.log('app rodando na porta ' + PORT);
+});
